1898dabc863e8e073938328afcc55707895e5d35
Ankit Anand <ankit.anand@oracle.com>
2022-01-09 02:53:19 +0530

    chor:refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: {
-    'dist/standalone/index': './modules/core/src/test/standalone/index.js',
-    'modules/browser-extension/popup/popup': './modules/browser-extension/src/main/popup/popup.js'
+    'standalone/index': './modules/core/src/test/standalone/index.js',
+    'browser-extension/popup/popup': './modules/browser-extension/src/main/popup/popup.js'
   },
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "modules/browser-extension/entry", to: "browser-extension" },
+        { context: "node_modules/@decentralized-identity/ion-tools/dist", from:"ion.min.js", to: "browser-extension/popup/node_modules/@decentralized-identity/ion-tools/dist" }
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
   //mode: "development",
   watch: true
 };
